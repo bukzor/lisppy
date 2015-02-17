b8472f6722f623843e0c831a7c05528c86f3268d
Buck Evan <buck.2019@gmail.com>
2015-02-16 20:54:24 -0800

    add atom commentary

--- lisppy/atom.py
+++ lisppy/atom.py
@@ -2,6 +2,23 @@ from .misc import getstate
 
 
 class Atom(object):
+    """For atomic symbols, we shall use strings of capital Latin letters and
+    digits with single imbedded blanks. (1995 remark: Imbedded blanks could be
+    allowed within symbols, because lists were then written with commas between
+    elements.)
+
+    Examples of atomic symbols are
+
+        A
+        ABA
+        APPLE PIE NUMBER 3
+
+    The symbols are atomic in the sense that any substructure they may have as
+    sequences of characters is ignored. We assume only that different symbols
+    can be distinguished.
+
+    section 3a, page 8
+    """
     def __init__(self, name):
         self.name = name
 
@@ -12,7 +29,10 @@ class Atom(object):
         return self.name
 
     def __eq__(self, other):
-        return isinstance(other, Atom) and getstate(self) == getstate(other)
+        if isinstance(other, Atom):
+            return getstate(self) == getstate(other)
+        else:
+            return NotImplemented
 
     def __ne__(self, other):
         return not self == other
