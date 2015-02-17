f97a17ba056b67fc2d136e8112e86a7d25df115c
Buck Evan <buck.2019@gmail.com>
2015-02-17 08:41:34 -0800

    add tests and commentary, thru page 13

--- lisppy/functions.py
+++ lisppy/functions.py
@@ -11,19 +11,36 @@ def atom(x):
         atom [X] = T
         atom [(X · A)] = F
 
-    section 3c, page 10
+    section 3c1, page 10
     """
     return T if isinstance(x, Atom) else F
 
 
 def eq(x, y):
-    if atom(x) is T and atom(y) is T:
-        return T if repr(x) == repr(y) else F
+    """eq [x;y] is defined if and only if both x and y are atomic. eq [x; y]
+    = T if x and y are the same symbol, and eq [x; y] = F otherwise. Thus
+
+        eq [X; X] = T
+        eq [X; A] = F
+        eq [X; (X · A)] is undefined.
+
+    section 3c2, page 10
+    """
+    if atom(x) is atom(y) is T:
+        return T if x == y else F
     else:
         return undefined
 
 
 def car(x):
+    """car[x] is defined if and only if x is not atomic.
+    Thus car [X] is undefined.
+
+        car [(X · A)] = X
+        car [((X · A) · Y )] = (X · A)
+
+    section 3c3, page 11
+    """
     if atom(x) is F:
         return x.first
     else:
@@ -31,15 +48,39 @@ def car(x):
 
 
 def cdr(x):
-    assert atom(x) is F
-    return x.second
+    """cdr [x] is also defined when x is not atomic.
+    Thus cdr [X] is undefined.
+
+        cdr [(X · A)] = A
+        cdr [((X · A) · Y )] = Y
+
+    section 3c4, page 11
+    """
+    if atom(x) is F:
+        return x.second
+    else:
+        return undefined
 
 
 def cons(x, y):
+    """cons [x; y] is defined for any x and y.
+
+        cons [X; A] = (X A)
+        cons [(X · A); Y ] = ((X · A)Y )
+
+    section 3c5, page 11
+    """
     return S(x, y)
 
 
 def ff(x):
+    """The value of ff[x] is the first atomic symbol of the S-expression x with
+    the parentheses ignored. Thus
+
+        ff[((A · B) · C)] = A
+
+    section 3d1, page 12
+    """
     if atom(x) is T:
         return x
     else:
@@ -47,7 +88,15 @@ def ff(x):
 
 
 def subst(x, y, z):
-    """return z with all y replaced with x"""
+    """This function gives the result of substituting the S-expression
+    x for all occurrences of the atomic symbol y in the S-expression z.
+
+    As an example, we have
+
+        subst[(X · A); B; ((A · B) · C)] = ((A · (X · A)) · C)
+
+    section 3d2, page 13
+    """
     if atom(z) is T:
         if eq(z, y) is T:
             return x
@@ -61,20 +110,31 @@ def subst(x, y, z):
 
 
 def equal(x, y):
-    return (
-        atom(x) and
-        atom(y) and
-        eq(x, y)
+    """This is a predicate that has the value T if x and y are the
+    same S-expression, and has the value F otherwise.
+
+    section 3d3, page 13
+    """
+    return T if (
+        atom(x) is
+        atom(y) is
+        eq(x, y) is
+        T
     ) or (
-        not atom(x) and
-        not atom(y) and
-        equal(car(x), car(y)) and
-        equal(cdr(x), cdr(y))
-    )
+        atom(x) is atom(y) is F and
+        equal(car(x), car(y)) is
+        equal(cdr(x), cdr(y)) is
+        T
+    ) else F
 
 
 def null(x):
-    return atom(x) and eq(x, NIL)
+    """This predicate is useful in dealing with lists.
+
+    section 3d3, page 13
+    """
+
+    return T if atom(x) is eq(x, NIL) is T else F
 
 
 def subst2(x, y, z):
@@ -108,20 +168,22 @@ def append(x, y):
 
 
 def among(x, y):
-    return not null(y) and (
-        equal(x, car(y)) or
-        among(x, cdr(y))
+    return null(y) is F and (
+        equal(x, car(y)) is T or
+        among(x, cdr(y)) is T
     )
 
 
 def pair(x, y):
-    if null(x) is T and null(y) is T:
+    if null(x) is null(y) is T:
         return NIL
-    elif atom(x) is F and atom(y) is F:
+    elif atom(x) is atom(y) is F:
         return cons(
             cons(car(x), car(y)),
             pair(cdr(x), cdr(y)),
         )
+    else:
+        return undefined
 
 # TODO: assert example
 
--- lisppy/test/functions_test.py
+++ lisppy/test/functions_test.py
@@ -1,7 +1,9 @@
 from lisppy.atom import Atom
 from lisppy.lists import S
 from lisppy.constants import undefined, NIL, T, F
-from lisppy import functions
+from lisppy.functions import(
+    atom, eq, car, cdr, cons, ff, subst, equal, null, sublis,
+)
 
 A, B, C, X, Y = (
     Atom('A'), Atom('B'), Atom('C'), Atom('X'), Atom('Y'),
@@ -9,19 +11,78 @@ A, B, C, X, Y = (
 
 
 def test_atom():
-    "section 3c, page 10"
-    assert functions.atom(X) is T
-    assert functions.atom(S(X, Y)) is F
+    "section 3c1, page 10"
+    assert atom(X) is T
+    assert atom(S(X, Y)) is F
 
 
 def test_eq():
-    assert functions.eq(X, X) is T
-    assert functions.eq(X, A) is F
-    assert functions.eq(X, S(X, A)) is undefined
+    "section 3c2, page 10"
+    assert eq(X, X) is T
+    assert eq(X, A) is F
+    assert eq(X, S(X, A)) is undefined
+
+
+def test_car():
+    "section 3c3, page 11"
+    assert car(X) is undefined
+    assert car(S(X, A)) is X
+    assert car(S(S(X, A), Y)) == S(X, A)
+
+
+def test_cdr():
+    "section 3c4, page 11"
+    assert cdr(X) is undefined
+    assert cdr(S(X, A)) is A
+    assert cdr(S(S(X, A), Y)) is Y
+
+
+def test_cons():
+    "section 3c5, page 11"
+    assert cons(X, A) == S(X, A)
+    assert cons(S(X, A), Y) == S(S(X, A), Y)
+
+
+def test_relation():
+    "section 3c, page 11"
+    assert car(cons(X, Y)) is X
+    assert cdr(cons(X, Y)) is Y
+
+    x = S(A, B)
+    assert cons(car(x), cdr(x)) == x
+
+
+def test_ff():
+    "section 3d1 page 12"
+    assert ff(S(S(A, B), C)) is A
+
+
+def test_subst():
+    "section 3d2 page 12"
+    assert subst(S(X, A), B, S(S(A, B), C)) == S(S(A, S(X, A)), C)
+
+
+def test_equal():
+    "section 3d3, page 13"
+    assert equal(A, A) is T
+    assert equal(A, B) is F
+    assert equal(A, S(A, B)) is F
+
+    assert equal(S(A, B), S(A, B)) is T
+    assert equal(S(A, B), S(A, A)) is F
+    assert equal(S(A, B), S(B, B)) is F
+    assert equal(S(A, B), A) is F
+
+
+def test_null():
+    "section 3d3, page 13"
+    assert null(NIL) is T
+    assert null(X) is F
+    assert null(S(X, Y)) is F
 
 
 def test_sublis():
-    left = functions.sublis(
+    left = sublis(
         S(S(X, S(S(A, B), NIL)),
           S(S(Y, S(S(B, C), NIL)),
             NIL)),
